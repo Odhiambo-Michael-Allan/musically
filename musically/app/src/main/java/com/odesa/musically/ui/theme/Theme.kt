7ed8c3e27ed9839033024292dbe02f9c14143ed7
Odhiambo-Michael-Allan <odhiambomichaelallan@gmail.com>
2024-01-20 23:39:47 +0300

    Latest Changes

--- musically/app/src/main/java/com/odesa/musically/ui/theme/Theme.kt
+++ musically/app/src/main/java/com/odesa/musically/ui/theme/Theme.kt
@@ -4,10 +4,8 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
@@ -28,30 +26,15 @@ enum class ThemeMode {
     SYSTEM_BLACK,
     LIGHT,
     DARK,
-    BLACK
+    BLACK,
+
 }
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+enum class ColorSchemeMode {
+    LIGHT,
+    DARK,
+    BLACK
+}
 
 @Composable
 fun MusicallyTheme(
@@ -61,21 +44,32 @@ fun MusicallyTheme(
     uiState: SettingsScreenUiState,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorSchemeMode = uiState.themeMode.toColorSchemeMode( isSystemInDarkTheme() )
+    val colorScheme = if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && uiState.useMaterialYou ) {
+        val context = LocalContext.current
+        when ( colorSchemeMode ) {
+            ColorSchemeMode.LIGHT -> dynamicLightColorScheme( context )
+            ColorSchemeMode.DARK -> dynamicDarkColorScheme( context )
+            ColorSchemeMode.BLACK -> ThemeColorSchemes.toBlackColorScheme(
+                dynamicDarkColorScheme( context )
+            )
+        }
+    } else {
+        val primaryColor = ThemeColors.resolvePrimaryColorName( uiState.primaryColorName )
+        when ( colorSchemeMode ) {
+            ColorSchemeMode.LIGHT -> ThemeColorSchemes.createLightColorScheme( primaryColor )
+            ColorSchemeMode.DARK -> ThemeColorSchemes.createDarkColorScheme( primaryColor )
+            ColorSchemeMode.BLACK -> ThemeColorSchemes.createBlackColorScheme( primaryColor )
+        }
     }
+
     val view = LocalView.current
-    if (!view.isInEditMode) {
+    if ( !view.isInEditMode ) {
         SideEffect {
             val window = ( view.context as Activity ).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController( window, view ).isAppearanceLightStatusBars = colorSchemeMode == ColorSchemeMode.LIGHT
         }
     }
 
@@ -108,4 +102,12 @@ fun ThemeMode.resolveName( language: Language ) = when ( this ) {
     ThemeMode.LIGHT -> language.light
     ThemeMode.DARK -> language.dark
     ThemeMode.BLACK -> language.black
+}
+
+fun ThemeMode.toColorSchemeMode( isSystemInDarkTheme: Boolean ) = when ( this ) {
+    ThemeMode.SYSTEM -> if ( isSystemInDarkTheme ) ColorSchemeMode.DARK else ColorSchemeMode.LIGHT
+    ThemeMode.SYSTEM_BLACK -> if ( isSystemInDarkTheme ) ColorSchemeMode.BLACK else ColorSchemeMode.LIGHT
+    ThemeMode.LIGHT -> ColorSchemeMode.LIGHT
+    ThemeMode.DARK -> ColorSchemeMode.DARK
+    ThemeMode.BLACK -> ColorSchemeMode.BLACK
 }
